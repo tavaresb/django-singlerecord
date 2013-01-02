e23f65275088812a8ade81dda4ed042db8eee56d
Bruno Tavares <eu@brunotavares.net>
2013-01-02 18:18:01 +0000

    version bump after the bugfixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-singlerecord',
-    version='0.1.0',
+    version='0.1.1',
     description='Abstraction for models with only one record for Django',
     author='Bruno Tavares',
     author_email='eu@brunotavares.net',
